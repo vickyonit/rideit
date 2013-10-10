0824bd51588bafab140f1d2a4c817ecebca103e8
Vignesh Rajendran <vignesh.theemperor@gmail.com>
2013-10-10 19:22:12 +0530

    Initital Commit by Vignesh Rajendran

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rideit::Application.config.secret_token = '15cb0c42b0959062239d670d208c63a0579b8a4b629dc0c497f7a883e5bf517ee5ea0f34a8838f2be30e14e4ef871ef3e07c0c716e6fabb790f1d1d24296ab82'
